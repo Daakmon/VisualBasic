234e462531331513cb1d8e91d89b1169493431b7
Daakmon <Daakmon94@hotmail.es>
2021-10-04 11:33:33 +0200

    Añadir
    Proyectos

---
+++ Juegos/Juegos/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LSB_ListaJuegos.SelectedIndexChanged
+
+        LBL_Titulo.Text = LSB_ListaJuegos.SelectedItem
+        PCB_Imagen.Image = IML_ListaImagenes.Images(LSB_ListaJuegos.SelectedIndex)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LSB_ListaJuegos.SelectedIndex = 0
+    End Sub
+
+    Private Sub PCB_Imagen_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
---
+++ Regalos/Regalos/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Regalos")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Regalos")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("359583b5-8968-467a-8b5b-aeee84bd5c25")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
