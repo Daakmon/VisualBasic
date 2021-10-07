4283e4367fafbe95b4cbde4462ef9aab14513963
Daakmon <Daakmon94@hotmail.es>
2021-10-07 10:17:59 +0200

    VB
    Primitiva

---
+++ Primitiva/Primitiva/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Primitiva
+    Private Sub Primitiva_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim chkControl As CheckBox
+
+        For Each chkControl In GRB_Numeros.Controls
+            AddHandler chkControl.CheckedChanged, AddressOf seleccionNumeros
+        Next
+    End Sub
+
+    Private Sub seleccionNumeros(sender As Object, e As EventArgs)
+
+    End Sub
+
+End Class
