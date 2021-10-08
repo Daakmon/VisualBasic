4c91854eec9550b247359c936eb408df09462d19
Daakmon <Daakmon94@hotmail.es>
2021-10-08 14:10:25 +0200

    VB
    Primitiva

--- Primitiva/Primitiva/Form1.Designer.vb
+++ Primitiva/Primitiva/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Primitiva
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Primitiva
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.GRB_Numeros = New System.Windows.Forms.GroupBox()
         Me.CHK_49 = New System.Windows.Forms.CheckBox()
@@ -74,15 +74,15 @@ Partial Class Primitiva
         Me.CHK_1 = New System.Windows.Forms.CheckBox()
         Me.BTN_Sorteo = New System.Windows.Forms.Button()
         Me.TXT_1 = New System.Windows.Forms.TextBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TXT_2 = New System.Windows.Forms.TextBox()
+        Me.TXT_3 = New System.Windows.Forms.TextBox()
+        Me.TXT_4 = New System.Windows.Forms.TextBox()
+        Me.TXT_5 = New System.Windows.Forms.TextBox()
+        Me.TXT_6 = New System.Windows.Forms.TextBox()
+        Me.BTN_Deshacer = New System.Windows.Forms.Button()
         Me.BTN_Salir = New System.Windows.Forms.Button()
         Me.LBL_Aciertos = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.LBL_ContAciertos = New System.Windows.Forms.Label()
         Me.GRB_Numeros.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -697,57 +697,63 @@ Partial Class Primitiva
         Me.TXT_1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TXT_1.Location = New System.Drawing.Point(140, 200)
         Me.TXT_1.Name = "TXT_1"
+        Me.TXT_1.ReadOnly = True
         Me.TXT_1.Size = New System.Drawing.Size(28, 26)
         Me.TXT_1.TabIndex = 1
         '
-        'TextBox1
+        'TXT_2
         '
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(174, 199)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(28, 26)
-        Me.TextBox1.TabIndex = 3
+        Me.TXT_2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXT_2.Location = New System.Drawing.Point(174, 199)
+        Me.TXT_2.Name = "TXT_2"
+        Me.TXT_2.ReadOnly = True
+        Me.TXT_2.Size = New System.Drawing.Size(28, 26)
+        Me.TXT_2.TabIndex = 3
         '
-        'TextBox2
+        'TXT_3
         '
-        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(208, 199)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(28, 26)
-        Me.TextBox2.TabIndex = 4
+        Me.TXT_3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXT_3.Location = New System.Drawing.Point(208, 199)
+        Me.TXT_3.Name = "TXT_3"
+        Me.TXT_3.ReadOnly = True
+        Me.TXT_3.Size = New System.Drawing.Size(28, 26)
+        Me.TXT_3.TabIndex = 4
         '
-        'TextBox3
+        'TXT_4
         '
-        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox3.Location = New System.Drawing.Point(242, 199)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(28, 26)
-        Me.TextBox3.TabIndex = 5
+        Me.TXT_4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXT_4.Location = New System.Drawing.Point(242, 199)
+        Me.TXT_4.Name = "TXT_4"
+        Me.TXT_4.ReadOnly = True
+        Me.TXT_4.Size = New System.Drawing.Size(28, 26)
+        Me.TXT_4.TabIndex = 5
         '
-        'TextBox4
+        'TXT_5
         '
-        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox4.Location = New System.Drawing.Point(276, 199)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(28, 26)
-        Me.TextBox4.TabIndex = 6
+        Me.TXT_5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXT_5.Location = New System.Drawing.Point(276, 199)
+        Me.TXT_5.Name = "TXT_5"
+        Me.TXT_5.ReadOnly = True
+        Me.TXT_5.Size = New System.Drawing.Size(28, 26)
+        Me.TXT_5.TabIndex = 6
         '
-        'TextBox5
+        'TXT_6
         '
-        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox5.Location = New System.Drawing.Point(310, 199)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(28, 26)
-        Me.TextBox5.TabIndex = 7
+        Me.TXT_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXT_6.Location = New System.Drawing.Point(310, 199)
+        Me.TXT_6.Name = "TXT_6"
+        Me.TXT_6.ReadOnly = True
+        Me.TXT_6.Size = New System.Drawing.Size(28, 26)
+        Me.TXT_6.TabIndex = 7
         '
-        'Button1
+        'BTN_Deshacer
         '
-        Me.Button1.Location = New System.Drawing.Point(42, 247)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 27)
-        Me.Button1.TabIndex = 8
-        Me.Button1.Text = "SORTEO"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.BTN_Deshacer.Location = New System.Drawing.Point(42, 247)
+        Me.BTN_Deshacer.Name = "BTN_Deshacer"
+        Me.BTN_Deshacer.Size = New System.Drawing.Size(75, 27)
+        Me.BTN_Deshacer.TabIndex = 8
+        Me.BTN_Deshacer.Text = "DESHACER"
+        Me.BTN_Deshacer.UseVisualStyleBackColor = True
         '
         'BTN_Salir
         '
@@ -769,29 +775,30 @@ Partial Class Primitiva
         Me.LBL_Aciertos.TabIndex = 10
         Me.LBL_Aciertos.Text = "ACIERTOS:"
         '
-        'Button2
+        'LBL_ContAciertos
         '
-        Me.Button2.Location = New System.Drawing.Point(228, 250)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 11
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.LBL_ContAciertos.AutoSize = True
+        Me.LBL_ContAciertos.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_ContAciertos.Location = New System.Drawing.Point(114, 294)
+        Me.LBL_ContAciertos.Name = "LBL_ContAciertos"
+        Me.LBL_ContAciertos.Size = New System.Drawing.Size(0, 13)
+        Me.LBL_ContAciertos.TabIndex = 11
         '
         'Primitiva
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(534, 326)
-        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.LBL_ContAciertos)
         Me.Controls.Add(Me.LBL_Aciertos)
         Me.Controls.Add(Me.BTN_Salir)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox5)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.BTN_Deshacer)
+        Me.Controls.Add(Me.TXT_6)
+        Me.Controls.Add(Me.TXT_5)
+        Me.Controls.Add(Me.TXT_4)
+        Me.Controls.Add(Me.TXT_3)
+        Me.Controls.Add(Me.TXT_2)
         Me.Controls.Add(Me.BTN_Sorteo)
         Me.Controls.Add(Me.TXT_1)
         Me.Controls.Add(Me.GRB_Numeros)
@@ -856,13 +863,13 @@ Partial Class Primitiva
     Friend WithEvents CHK_1 As CheckBox
     Friend WithEvents BTN_Sorteo As Button
     Friend WithEvents TXT_1 As TextBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents TXT_2 As TextBox
+    Friend WithEvents TXT_3 As TextBox
+    Friend WithEvents TXT_4 As TextBox
+    Friend WithEvents TXT_5 As TextBox
+    Friend WithEvents TXT_6 As TextBox
+    Friend WithEvents BTN_Deshacer As Button
     Friend WithEvents BTN_Salir As Button
     Friend WithEvents LBL_Aciertos As Label
-    Friend WithEvents Button2 As Button
+    Friend WithEvents LBL_ContAciertos As Label
 End Class
--- Primitiva/Primitiva/Form1.vb
+++ Primitiva/Primitiva/Form1.vb
@@ -1,14 +1,82 @@
 ﻿Public Class Primitiva
+    Dim rndm, cont As Integer
+
     Private Sub Primitiva_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim chkControl As CheckBox
 
         For Each chkControl In GRB_Numeros.Controls
             AddHandler chkControl.CheckedChanged, AddressOf seleccionNumeros
         Next
+
+        'LBL_ContAciertos.Text = ""
+        BTN_Sorteo.Enabled = False
     End Sub
 
     Private Sub seleccionNumeros(sender As Object, e As EventArgs)
+        Dim seleccion As Integer
+        seleccion = 0
 
+        For Each ck In GRB_Numeros.Controls
+            If ck.Checked Then
+                seleccion += 1
+            End If
+        Next
+
+        If seleccion = 6 Then
+            BTN_Sorteo.Enabled = True
+        ElseIf seleccion > 6 Then
+            sender.Checked = False
+            MsgBox("Ya se han seleccionado 6 numeros")
+        End If
+    End Sub
+
+    Private Sub Random()
+        Randomize()
+
+        rndm = Int(Rnd() * 49) + 1
+    End Sub
+
+    Private Sub BTN_Sorteo_Click(sender As Object, e As EventArgs) Handles BTN_Sorteo.Click
+        cont = 0
+
+        For Each numero In Me.Controls
+            If TypeOf numero Is TextBox Then
+                Random()
+                numero.Text = rndm
+            End If
+        Next
+
+        For Each ck In GRB_Numeros.Controls
+            For Each numero In Me.Controls
+                If TypeOf numero Is TextBox Then
+                    If numero.Text = ck.Text Then
+                        cont += 1
+                        LBL_ContAciertos.Text = cont
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub BTN_Deshacer_Click(sender As Object, e As EventArgs) Handles BTN_Deshacer.Click
+        cont = 0
+
+        For Each numero In Me.Controls
+            If TypeOf numero Is TextBox Then
+                numero.Text = ""
+            End If
+        Next
+
+        For Each ck In GRB_Numeros.Controls
+            ck.Checked = False
+        Next
+
+        BTN_Sorteo.Enabled = False
+        LBL_ContAciertos.Text = ""
+    End Sub
+
+    Private Sub BTN_Salir_Click(sender As Object, e As EventArgs) Handles BTN_Salir.Click
+        End
     End Sub
 
 End Class
